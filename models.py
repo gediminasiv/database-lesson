797e99c75d51681620fe69ad01ffdf250c5196ee
gediminasiv <gediminasiv>
2019-05-06 17:07:23 +0300

    Add updates for model

---
+++ models.py
@@ -0,0 +1,8 @@
+from smartninja_nosql.odm import Model
+
+class User(Model):
+    def __init__(self, name, email, **kwargs):
+        self.name = name
+        self.email = email
+
+        super().__init__(**kwargs)
